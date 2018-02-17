2a927b7c7c7463662774c9bf1ae93a9bf319a928
Andreas Senfter <asenfter@gmail.com>
2018-02-17 01:19:27 +0100

    first version co1n5 token

--- src/Math.sol
+++ src/Math.sol
@@ -1,30 +1,37 @@
 pragma solidity 0.4.11;
 
-/**
- * Save math operations
- */
+// Save math operations
 contract Math {
 
-  function safeMul(uint256 a, uint256 b) internal returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+    function safeMul(uint256 a, uint256 b) internal returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
 
-  function safeDiv(uint256 a, uint256 b) internal returns (uint256) {
-    assert(b > 0);
-    uint256 c = a / b;
-    assert(a == b * c + a % b);
-    return c;
-  }
+    function safeDiv(uint256 a, uint256 b) internal returns (uint256) {
+        assert(b > 0);
+        uint256 c = a / b;
+        assert(a == b * c + a % b);
+        return c;
+    }
 
-  function safeSub(uint256 a, uint256 b) internal returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-  function safeAdd(uint256 a, uint256 b) internal returns (uint256) {
-    uint c = a + b;
-    assert(a<=c && b<=c);
-    return c;
-  }
+    function safeSub(uint256 a, uint256 b) internal returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function safeAdd(uint256 a, uint256 b) internal returns (uint256) {
+        uint c = a + b;
+        assert(a <= c && b <= c);
+        return c;
+    }
+
+    // mitigate short address attack
+    // thanks to https://github.com/numerai/contract/blob/c182465f82e50ced8dacb3977ec374a892f5fa8c/contracts/Safe.sol#L30-L34.
+    // TODO: doublecheck implication of >= compared to ==
+    modifier onlyPayloadSize(uint numWords) {
+        assert(msg.data.length >= numWords * 32 + 4);
+        _;
+    }
 }
